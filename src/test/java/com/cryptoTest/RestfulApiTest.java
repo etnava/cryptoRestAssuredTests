fc1fd008df60e499e16f3ffadf95231e7d2bd751
Edward S <38728269+etnava@users.noreply.github.com>
2020-05-01 16:39:50 +1000

    fixed more tests

--- src/test/java/com/cryptoTest/RestfulApiTest.java
+++ src/test/java/com/cryptoTest/RestfulApiTest.java
@@ -1,14 +1,7 @@
 package com.cryptoTest;
 
-import static org.junit.Assert.assertEquals;
 
 import org.junit.*;
-
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -16,37 +9,59 @@ import static org.hamcrest.Matchers.*;
 
 public class RestfulApiTest {
 
-	
-	
+	@Before
+	public void init() {
+		baseURI = "http://localhost:2222/";
+	}
+
 	@Test
 	public void that_base_request_returnsOK() {
-		RestAssured.baseURI = "http://localhost:1111";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.get("");
-		int statusCode = response.getStatusCode();
-		assertEquals(statusCode,  200);
+		given().get().then().statusCode(200);
 	}
 
-	
 	@Test
-	public void that_requested_id_returnsOK() {
-		RestAssured.baseURI = "http://localhost:1111";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.get("/bitcoin");
-		int statusCode = response.getStatusCode();
-		assertEquals(statusCode,  200);
-	}
-	
-	@Test
-	public void that_response_requested_bitcoin_returns_id_OK() {
-		RestAssured.baseURI = "http://localhost:1111";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.get("/bitcoin");
-		JsonPath jsonPathEvaluator = response.jsonPath();
-		String id  = jsonPathEvaluator.getString("id");
-		assertEquals("bitcoin", id);
+	public void that_bitcoin_request_returnOK() {
+		given().get("bitcoin").then().statusCode(200);
 	}
 
+	@Test
+	public void that_bitcoin_id_return_bitcoin() {
+		given().get("bitcoin").then().body("id", equalTo("bitcoin"));
+	}
+
+	@Test
+	public void that_header_is_application_json() {
+		given().get().then().contentType("application/json");
+	}
+
+	@Test
+	public void that_bitcoin_header_is_application_json() {
+		given().get("bitcoin").then().contentType("application/json");
+	}
+
+	@Test
+	public void that_response_time_is_OK() {
+		given().get().then().time(lessThan(1000L));
+	}
+
+	@Test
+	public void that_bitcoin_response_time_is_OK() {
+		given().get("bitcoin").then().time(lessThan(1000L));
+	}
+
+	@Test
+	public void that_bitcoin_market_cap_is_correct() {
+		given().get("bitcoin").then().body("market_cap", equalTo("246842812268"));
+	}
 	
+	@Test
+	public void that_bitcoin_market_price_is_correct() {
+		given().get("bitcoin").then().body("current_price", equalTo(13448.54f));
+	}
+	
+	@Test
+	public void that_api_returns_list_of_10() {
+		given().get().then().body("size()", is(10));
+	}
 	
 }
